c04c73d30d2f26c3d437a0fdb3699b3ba7a5f63a
anurag7033 <122593785+anurag7033@users.noreply.github.com>
2026-01-01 10:05:07 +0000

    Add gh-pages deploy script

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,21 @@
-import { defineConfig } from "vite"
-import react from "@vitejs/plugin-react"
-import path from "path"
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: "/",
   plugins: [react()],
+
+  // CUSTOM DOMAIN = ROOT PATH
+  base: "/",
+
+  build: {
+    outDir: "dist",
+    emptyOutDir: true,
+  },
+
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      "@": "/src",
     },
   },
-  build: {
-    outDir: "docs",
-  },
-})
+});
